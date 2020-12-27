411210bbcf36cf6c1030fc7dcffd6034a154198a
Angela Castillo <angiekroll@gmail.com>
2020-12-27 13:53:01 -0500

    add validation roadgenerator 12D

--- src/main/java/com/jw/censo/service/DataFilterService.java
+++ src/main/java/com/jw/censo/service/DataFilterService.java
@@ -68,14 +68,16 @@ public class DataFilterService {
                 addressFormat = addressFormat.replaceAll("#", "");
                 String[] adressArray = addressFormat.split(" "); // convierte la direccion en un array de string
                 String[] adressArrayFormat = Arrays.stream(adressArray).filter(chart -> !chart.isEmpty()).toArray(String[]::new); // genera nuevo array eliminando posiciones vacias
-                if (adressArrayFormat[1].length() <= 3) {  // valida que no vengan mas de tres caracteres en el numero de la calle
+                if (adressArrayFormat[1].length() <= 3 && adressArrayFormat[2].length() <= 3) {  // valida que no vengan mas de tres caracteres en el numero de la calle
                     String mainRoadNumber = validateMainRoadNumber(adressArrayFormat);
                     if (adressArrayFormat[1].length() < 3 || Integer.parseInt(mainRoadNumber) != 31){  /// para validar las calles 31 sin ninguna letra
                         String roadGeneratorNumber = validateRoadGeneratorNumber(adressArrayFormat);
                         if (Integer.parseInt(mainRoadNumber) >= 22 && Integer.parseInt(mainRoadNumber) <= 31) {  // valida el rango del territorio para calles
-                            //   roadGeneratorNumber = adressArrayFormat[2].substring(0, 2);
                             if (Integer.parseInt(roadGeneratorNumber) >= 12 && Integer.parseInt(roadGeneratorNumber) <= 30) { // valida el rango del territorio para carrera correspondiente a la calle anterior
-                                personsFilter.add(person);
+                                char letra = adressArrayFormat[2].charAt(2);
+                                if(letra >= 'D') {  //validacion roadGeneratorNumber (carrera) 12D
+                                    personsFilter.add(person);
+                                }
                             }
                         }
                     }
@@ -101,18 +103,19 @@ public class DataFilterService {
                 addressFormat = addressFormat.replaceAll("#", "");
                 String[] adressArray = addressFormat.split(" "); // convierte la direccion en un array de string
                 String[] adressArrayFormat = Arrays.stream(adressArray).filter(chart -> !chart.isEmpty()).toArray(String[]::new); // genera nuevo array eliminando posiciones vacias
-                if (adressArrayFormat[1].length() <= 3) {  // valida que no vengan mas de tres caracteres en el numero de la calle
+                if (adressArrayFormat[1].length() <= 3 && adressArrayFormat[2].length() <= 3) {  // valida que no vengan mas de tres caracteres en el numero de la calle
                     String mainRoadNumber = validateMainRoadNumber(adressArrayFormat);
                     String roadGeneratorNumber = validateRoadGeneratorNumber(adressArrayFormat);
                     if (Integer.parseInt(mainRoadNumber) >= 14 && Integer.parseInt(mainRoadNumber) <= 30) {  // valida el rango del territorio para calles
-                      //  roadGeneratorNumber = adressArrayFormat[2].substring(0, 2);
                         if (Integer.parseInt(roadGeneratorNumber) >= 22 && Integer.parseInt(roadGeneratorNumber) <= 31) { // valida el rango del territorio para carrera correspondiente a la calle anterior
                             personsFilter.add(person);
                         }
                     } else if (Integer.parseInt(mainRoadNumber) >= 12 && Integer.parseInt(mainRoadNumber) <= 30) {
-                       // roadGeneratorNumber = adressArrayFormat[2].substring(0, 2);
-                        if (Integer.parseInt(roadGeneratorNumber) >= 27 && Integer.parseInt(roadGeneratorNumber) <= 31) { // valida el rango del territorio para carrera correspondiente a la calle anterior
-                            personsFilter.add(person);
+                        char letra = adressArrayFormat[1].charAt(2);
+                        if(letra >= 'D'){  //validacion roadmain (carrera) 12D
+                            if (Integer.parseInt(roadGeneratorNumber) >= 27 && Integer.parseInt(roadGeneratorNumber) <= 31) { // valida el rango del territorio para carrera correspondiente a la calle anterior
+                                personsFilter.add(person);
+                            }
                         }
                     }
                 }
@@ -150,6 +153,7 @@ public class DataFilterService {
         return mainRoadNumber;
     }
 
+
     public String validateRoadGeneratorNumber(String[] adressArrayFormat) {
         String roadGeneratorNumber;
         if (adressArrayFormat[2].length() == 1) {
@@ -165,28 +169,31 @@ public class DataFilterService {
         return roadGeneratorNumber;
     }
 
-    public String validateCarera12D(String string){
-        if(Integer.parseInt(string) == 12){
 
-
-        }
-        return null;
-    }
-
-
-
-
-
-
-    //TODO: 1. OBTENER TODA LA DATA.
+    //TODO PROCESAMIENTO:
+    // 1. OBTENER TODA LA DATA.
     // 2: FILTRAR POR CARDINALIDAD "SUR" Y ELIMINAR LOS "ESTE".
-    // 3: ABREVIATURAS: CL, CLL, CALLE, AC  --> CALLES
-    //                  CR, CRA, KR, CARRERA, AK   ---> CARRERAS
-    //                  DG, AV, TV  --> INVESTIGACION DENTRO DEL TERRITORIO EN MAPS
+    // 3: ABREVIATURAS: CL, CLL, CALLE, AC, DG  --> CALLES
+    //                  CR, CRA, KR, CARRERA, AK, TV   ---> CARRERAS
+    //                  AV  --> INVESTIGACION DENTRO DEL TERRITORIO EN MAPS
     // https://www.rankia.co/blog/dian/4269251-nomenclatura-dian-2020   --> Nomenclatura DIAN 2020 – Direcciones
     // CONDICIONES DE RANGOS TERRITORIO OLAYA
     // Carrera	>= Kr 12 D	<= Kr 30
     // Calle >= Cll 22 Sur	<= Cll 31 Sur
+
+
+    //TODO PRECONDICIONES DEL ARCHIVO:
+    // Se debe unificar la letra que acompañe a la via principal o via generadora.  --> ejemplo: TV 18I BIS A 72 36 SUR
+    // Se deben quitar todos los "BIS A", despues de las vias principales para evitar una nueva posicion en el array.  --> ejemplo: cl 27A sur # 14B - 44
+    // Formateo a lenguaje SQL
+
+
+    //TODO PENDIENTE:
+    // Guardar el resultado en DB con un estado de migrado.
+    // Cambiad DB no volatil
+    // Cambiar por address
+    // reorganizar código
+    // validar quitar el indicativo
 
 
 }
